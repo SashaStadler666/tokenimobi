45c6c66ddfb8eb55ac5336f17a0cd2953c21fc02
SashaStadler666 <deadsec47@gmail.com>
2025-04-24 08:15:32 -0300

    Update mockTokens.ts

--- src/lib/models/mockData/mockTokens.ts
+++ src/lib/models/mockData/mockTokens.ts
@@ -1,14 +1,30 @@
+import { Token } from "@/lib/models";
 
-import { Token } from "../interfaces/TokenInterface";
-import { urbanTokens } from "./urbanTokens";
-import { ruralTokens } from "./ruralTokens";
-import { wholePropertyTokens } from "./wholePropertyTokens";
-import { kTokens } from "../Token";
-
-// Combine all fractional tokens into one array
-export const mockTokens = [...kTokens];
-const kTokens = [...];
-  ...urbanTokens,
-  ...ruralTokens,
-  ...kTokens
+const kTokens: Token[] = [
+  {
+    id: "1",
+    name: "Ulisses Vieira",
+    symbol: "K1",
+    fractionPrice: 0.01,
+    totalFractions: 1000,
+    availableFractions: 1000,
+    marketCap: 1550000,
+    volume24h: 10000,
+    priceChange24h: 2.5,
+    imageUrl: "https://link-da-imagem.com",
+  },
+  {
+    id: "2",
+    name: "Parintins",
+    symbol: "K2",
+    fractionPrice: 0.015,
+    totalFractions: 1000,
+    availableFractions: 1000,
+    marketCap: 1300000,
+    volume24h: 8500,
+    priceChange24h: 1.7,
+    imageUrl: "https://link-da-imagem.com",
+  },
 ];
+
+export const mockTokens = [...kTokens];
